cb6505491b4c17389b675f76061aafcab8ecad3f
Danesh Petigara <danesh.petigara@broadcom.com>
2017-06-12 12:53:38 -0700

    brcmstbwowlan: Wake-On-Wlan driver cleanup
    Cleanup driver so it conforms to the kernel driver structure and
    coding standards.
    
    refs #SWANDROID-3805
    
    Signed-off-by: Danesh Petigara <danesh.petigara@broadcom.com>

--- linux-4.1/drivers/net/wireless/brcmstbwowlan/brcmstb_wowlan.c
+++ linux-4.1/drivers/net/wireless/brcmstbwowlan/brcmstb_wowlan.c
@@ -1,144 +1,110 @@
-/******************************************************************************
- * Broadcom Proprietary and Confidential. (c)2016 Broadcom. All rights reserved.
+/*
+ * Wake-On-Wlan driver for Broadcom 7271 set top box SoC
  *
- * This program is the proprietary software of Broadcom and/or its
- * licensors, and may only be used, duplicated, modified or distributed pursuant
- * to the terms and conditions of a separate, written license agreement executed
- * between you and Broadcom (an "Authorized License").  Except as set forth in
- * an Authorized License, Broadcom grants no license (express or implied), right
- * to use, or waiver of any kind with respect to the Software, and Broadcom
- * expressly reserves all rights in and to the Software and all intellectual
- * property rights therein.  IF YOU HAVE NO AUTHORIZED LICENSE, THEN YOU
- * HAVE NO RIGHT TO USE THIS SOFTWARE IN ANY WAY, AND SHOULD IMMEDIATELY
- * NOTIFY BROADCOM AND DISCONTINUE ALL USE OF THE SOFTWARE.
+ * Copyright (c) 2017 Broadcom
  *
- * Except as expressly set forth in the Authorized License,
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation version 2.
  *
- * 1. This program, including its structure, sequence and organization,
- *    constitutes the valuable trade secrets of Broadcom, and you shall use all
- *    reasonable efforts to protect the confidentiality thereof, and to use
- *    this information only in connection with your use of Broadcom integrated
- *    circuit products.
- *
- * 2. TO THE MAXIMUM EXTENT PERMITTED BY LAW, THE SOFTWARE IS PROVIDED "AS IS"
- *    AND WITH ALL FAULTS AND BROADCOM MAKES NO PROMISES, REPRESENTATIONS OR
- *    WARRANTIES, EITHER EXPRESS, IMPLIED, STATUTORY, OR OTHERWISE, WITH RESPECT
- *    TO THE SOFTWARE.  BROADCOM SPECIFICALLY DISCLAIMS ANY AND ALL IMPLIED
- *    WARRANTIES OF TITLE, MERCHANTABILITY, NONINFRINGEMENT, FITNESS FOR A
- *    PARTICULAR PURPOSE, LACK OF VIRUSES, ACCURACY OR COMPLETENESS, QUIET
- *    ENJOYMENT, QUIET POSSESSION OR CORRESPONDENCE TO DESCRIPTION. YOU ASSUME
- *    THE ENTIRE RISK ARISING OUT OF USE OR PERFORMANCE OF THE SOFTWARE.
- *
- * 3. TO THE MAXIMUM EXTENT PERMITTED BY LAW, IN NO EVENT SHALL BROADCOM OR ITS
- *    LICENSORS BE LIABLE FOR (i) CONSEQUENTIAL, INCIDENTAL, SPECIAL, INDIRECT,
- *    OR EXEMPLARY DAMAGES WHATSOEVER ARISING OUT OF OR IN ANY WAY RELATING TO
- *    YOUR USE OF OR INABILITY TO USE THE SOFTWARE EVEN IF BROADCOM HAS BEEN
- *    ADVISED OF THE POSSIBILITY OF SUCH DAMAGES; OR (ii) ANY AMOUNT IN EXCESS
- *    OF THE AMOUNT ACTUALLY PAID FOR THE SOFTWARE ITSELF OR U.S. $1, WHICHEVER
- *    IS GREATER. THESE LIMITATIONS SHALL APPLY NOTWITHSTANDING ANY FAILURE OF
- *    ESSENTIAL PURPOSE OF ANY LIMITED REMEDY.
- ******************************************************************************/
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/platform_device.h>
 #include <linux/of.h>
 #include <linux/interrupt.h>
-#include <linux/kallsyms.h>
 
-int (*wl_resume_normalmode)(void);
-
-struct stbsoc_context {
-    void __iomem *regs;
-    int irq;
-    int wowl_irq;
-    struct platform_device *pdev;
+struct private_data {
+	void __iomem *regs;
+	int wowlan_irq;
+	struct platform_device *pdev;
 };
 
-static irqreturn_t wowl_isr(int irq, void *dev)
+static irqreturn_t brcmstb_wowlan_isr(int irq, void *dev)
 {
-    struct stbsoc_context *priv = dev;
-    pm_wakeup_event(&priv->pdev->dev, 0);
+	struct private_data *pdata = dev;
 
-    printk("#####wowl_isr######\n\n\n\n\n");
-    return 0;
+	pm_wakeup_event(&pdata->pdev->dev, 0);
+	dev_dbg(&pdata->pdev->dev,
+		"#####wowlan_isr######\n\n");
+
+	return 0;
 }
 
-static int stbsoc_wlan_remove(struct platform_device *pdev)
+static int brcmstb_wowlan_probe(struct platform_device *pdev)
 {
-    int ret;
-    struct stbsoc_context *priv;
+	struct private_data *pdata;
+	struct resource *res;
+	int ret;
 
-    priv = devm_kzalloc(&pdev->dev, sizeof(*priv), GFP_KERNEL);
+	pdata = devm_kzalloc(&pdev->dev, sizeof(*pdata), GFP_KERNEL);
+	if (!pdata)
+		return -ENOMEM;
 
-    priv->pdev = pdev;
-    priv->wowl_irq = platform_get_irq(pdev, 1);
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	pdata->regs = devm_ioremap_resource(&pdev->dev, res);
+	if (IS_ERR(pdata->regs)) {
+		dev_err(&pdev->dev, "couldn't map registers\n");
+		return PTR_ERR(pdata->regs);
+	}
 
-    disable_irq_wake(priv->wowl_irq);
+	pdata->pdev = pdev;
+	pdata->wowlan_irq = platform_get_irq(pdev, 1);
 
-    ret = device_set_wakeup_enable(&pdev->dev, 0);
-    device_set_wakeup_capable(&pdev->dev, 0);
-    return 0;
+	if (pdata->wowlan_irq < 0) {
+		dev_err(&pdev->dev, "couldn't obtain wowlan_irq\n");
+		return -EINVAL;
+	}
+
+	ret = devm_request_irq(&pdev->dev, pdata->wowlan_irq,
+		brcmstb_wowlan_isr, IRQF_NO_SUSPEND,
+		"wlan_wol", pdata);
+	if (ret) {
+		dev_err(&pdev->dev, "wowlan_irq request failure\n");
+		return ret;
+	}
+
+	device_set_wakeup_capable(&pdev->dev, true);
+	device_set_wakeup_enable(&pdev->dev, true);
+	enable_irq_wake(pdata->wowlan_irq);
+	platform_set_drvdata(pdev, pdata);
+
+	return 0;
 }
 
-static int stbsoc_wlan_probe(struct platform_device *pdev)
+static int brcmstb_wowlan_remove(struct platform_device *pdev)
 {
-    struct stbsoc_context *priv;
-    struct resource *r;
-    int ret;
+	struct private_data *pdata = platform_get_drvdata(pdev);
 
-    priv = devm_kzalloc(&pdev->dev, sizeof(*priv), GFP_KERNEL);
-    if (!priv)
-        return -ENOMEM;
+	disable_irq_wake(pdata->wowlan_irq);
+	device_set_wakeup_enable(&pdev->dev, false);
+	device_set_wakeup_capable(&pdev->dev, false);
+	platform_set_drvdata(pdev, NULL);
 
-    r = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-    priv->regs = devm_ioremap_resource(&pdev->dev, r);
-
-    if (IS_ERR(priv->regs))
-        return PTR_ERR(priv->regs);
-    priv->pdev = pdev;
-    priv->irq = platform_get_irq(pdev, 0);
-    priv->wowl_irq = platform_get_irq(pdev, 1);
-
-    if (priv->irq < 0 || priv->wowl_irq < 0)
-        return -EINVAL;
-
-    ret = devm_request_irq(&pdev->dev, priv->wowl_irq, wowl_isr, IRQF_NO_SUSPEND, "wlan_wol", priv);
-
-    device_set_wakeup_capable(&pdev->dev, 1);
-
-    ret = device_set_wakeup_enable(&pdev->dev, 1);
-    enable_irq_wake(priv->wowl_irq);
-    return 0;
+	return 0;
 }
 
-
-static const struct of_device_id stbsoc_wlan_of_match[] = {
-    { .compatible = "brcm,bcm7271-wlan" },
-    { /* sentinel */ },
+static const struct of_device_id brcmstb_wowlan_of_match[] = {
+	{ .compatible = "brcm,bcm7271-wlan" },
+	{ /* sentinel */ },
 };
 
-static struct platform_driver stbsoc_wlan_driver = {
-    .probe	= stbsoc_wlan_probe,
-    .remove = stbsoc_wlan_remove,
-    .driver = {
-        .name = "bcm7271-wlan",
-        .of_match_table = stbsoc_wlan_of_match,
-    },
+static struct platform_driver brcmstb_wowlan_driver = {
+	.probe	= brcmstb_wowlan_probe,
+	.remove = brcmstb_wowlan_remove,
+	.driver = {
+		.name = "bcm7271-wowlan",
+		.of_match_table = brcmstb_wowlan_of_match,
+	},
 };
-
-static int __init stbsoc_wlan_init(void)
-{
-    return platform_driver_register(&stbsoc_wlan_driver);
-}
-module_init(stbsoc_wlan_init);
-
-static void __exit stbsoc_wlan_exit(void)
-{
-    platform_driver_unregister(&stbsoc_wlan_driver);
-}
-module_exit(stbsoc_wlan_exit);
+module_platform_driver(brcmstb_wowlan_driver);
 
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Broadcom stbsoc WLAN SHIM");
+MODULE_DESCRIPTION("BCM7271 Wake-On-WLAN Driver");
 MODULE_AUTHOR("Broadcom");
