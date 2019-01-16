465b6cb06b5837b98eb6a0a8aeb18c19250b7b90
Jerry Wong <jerry.wong@broadcom.com>
2019-01-16 10:02:48 -0800

    kernel: sources for 15.01.2019 sync
    Based on efeaad8fe71cca79a57d62d678318c842569ec62

--- linux-4.9/drivers/net/ethernet/broadcom/genet/bcmmii.c
+++ linux-4.9/drivers/net/ethernet/broadcom/genet/bcmmii.c
@@ -400,9 +400,12 @@ int bcmgenet_mii_probe(struct net_device *dev)
 	_phy_pause_set(phydev, priv->pause_flags);
 
 	/* The internal PHY has its link interrupts routed to the
-	 * Ethernet MAC ISRs
+	 * Ethernet MAC ISRs. On GENETv5 there is a hardware issue
+	 * that prevents the signaling of link UP interrupts when
+	 * the link operates at 10Mbps, so fallback to polling for
+	 * those versions of GENET.
 	 */
-	if (priv->internal_phy)
+	if (priv->internal_phy && !GENET_IS_V5(priv))
 		dev->phydev->irq = PHY_IGNORE_INTERRUPT;
 
 	return 0;
